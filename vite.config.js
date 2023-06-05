5228c69427971b7d5c95a32fd2fece875012283d
Mahlori <achievermahlori@gmail.com>
2023-06-05 13:14:44 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from './data.json';
+import { defineConfig } from 'vite';
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input : {
+        main: 'index.html',
+        one: 'index-bootstrap.html',
+        tw0: 'index-materialize.html'
+      }
+    },
+  },
+    base : '/responsive_webpages/',   // <-- add this
+    plugins: [handlebars({
+      context : {
+          /*
+          previous setup here
+          */
+          data   // add this
+      }
+    })],
+  });
+
+
+
+
+
+
+
+
+
